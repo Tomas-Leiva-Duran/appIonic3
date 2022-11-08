dc4a01dba9c1e0acff5bbb0de1dc73df8428d6af
Tomas-Leiva-Duran <101905549+Tomas-Leiva-Duran@users.noreply.github.com>
2022-11-07 21:40:21 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'my.app_ionic3',
+  appName: 'my-app_ionic3',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
